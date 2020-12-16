9f2775c6493b4974c0d807a6136702b5f2fd5f30
Irinilg <irinilg2011@gmail.com>
2020-12-16 09:11:01 -0800

    2.3 Menu Changes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("Hello and welcome to my portfolio site! Please take a look around.");
+
+function menuToggle() {
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
+  }
+}
