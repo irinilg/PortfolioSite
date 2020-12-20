1566d98a402bab897d52ba572b5e9032f9dce441
Irinilg <irinilg2011@gmail.com>
2020-12-20 12:46:40 -0800

    2.5

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-console.log("Hello and welcome to my portfolio site! Please take a look around.");
+console.log('Hello and welcome to my portfolio site! Please take a look around.')
 
-function menuToggle() {
-  var x = document.getElementById("myNavtoggle");
-  if (x.className === "navtoggle") {
-    x.className += " responsive";
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive'
   } else {
-    x.className = "navtoggle";
+    x.className = 'navtoggle'
   }
 }
